6803d7b49ac4da0ac495b0f6593c7f0f722a4f12
GalaktionovMaksim <12021105186@irgups.ru>
2022-05-31 02:34:55 +0800

    sssssaasfa

--- laba7.js
+++ laba7.js
@@ -15,3 +15,27 @@ let popup = document.getElementById('mypopup'),
                 popup.style.display ="none";
             }
         }
+let offset =0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click',function(){
+    offset = offset + 500;
+    if (offset > 1500){
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click',function(){
+    offset = offset - 500;
+    if (offset < 0){
+        offset = 1500;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+var color = 'green';
+document.querySelector('button').onclick = function(){
+    console.log(this)  // теперь this это кнопка
+    this.style.background = "red";
+}
+
