7084ef2d064e96f8ff6b7da3d87cb029bb72effb
GalaktionovMaksim <12021105186@irgups.ru>
2022-05-31 00:26:53 +0800

    commit7lb

---
+++ laba7.js
@@ -0,0 +1,17 @@
+
+var dialog = document.querySelector('dialog');
+document.querySelector('#show').onclick = function() {
+  dialog.show();
+};
+document.querySelector('#close').onclick = function() {
+  dialog.close();
+};
+
+
+var dialog = document.querySelector('dialog');
+document.querySelector('#show').onclick = function() {
+  dialog.show();
+};
+document.querySelector('#close').onclick = function() {
+  dialog.close();
+};
